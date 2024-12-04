c4371a5623afd4f5a6925b0da72e698cfde3d71d
HajwaaN <kevin-hattina@hotmail.com>
2024-12-04 12:41:12 +0100

    Add project files.

---
+++ kevinhexeption/Program.cs
@@ -0,0 +1,34 @@
+﻿
+using System;
+
+namespace exeptionKevinHattina
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HandleMultipleExceptions("not a number", 1); // Skriv u Invalid format
+            HandleMultipleExceptions("2", 5);           // Skriv ut Index out of range
+            HandleMultipleExceptions("1", 1);           // Skriv ut 2
+        }
+
+        static void HandleMultipleExceptions(string input, int index)
+        {
+            int[] numbers = { 1, 2, 3 };
+
+            try
+            {
+                int parsedNumber = int.Parse(input);
+                Console.WriteLine(numbers[index]);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid format.");
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("Index out of range.");
+            }
+        }
+    }
+}
